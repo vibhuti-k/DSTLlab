cf332e49fa2097db60f24a1ad9d0803a73944757
vibhuti-k <97297327+vibhuti-k@users.noreply.github.com>
2022-09-27 15:54:34 +0530

    Update CartesionProduct.c

--- CartesionProduct.c
+++ CartesionProduct.c
@@ -1,23 +1,28 @@
 #include<stdio.h>
 void main()
 {
-    int a[100],b[100],i,q;
-    printf("Enter the number of elements in the set : ");
+    int a[100],b[100],i,j,q,p;
+    printf("Enter the number of elements in the set A : ");
     scanf("%d",&q);
-    printf("Enter the elements of the first set.\n");
+    printf("Enter the elements of the set A.\n");
     for(i=0;i<q;i++)
     {
         scanf("%d",&a[i]);
     }
-    printf("Enter the elements of the second set.\n");
-    for(i=0;i<q;i++)
+    printf("Enter the number of elements in the set B : ");
+    scanf("%d",&p);
+    printf("Enter the elements of the set B.\n");
+    for(i=0;i<p;i++)
     {
         scanf("%d",&b[i]);
     }
     printf("AxB={");
     for(i=0;i<q;i++)
     {
-        printf("(%d,%d),",a[i],b[i]);
+        for(j=0;j<p;j++)
+        {
+            printf("(%d,%d),",a[i],b[j]);
+        }
     }
     printf("}");
 }
